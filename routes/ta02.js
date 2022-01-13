5342bfbcfa8a659423fa7c2f01924f812030abfe
MichaelSilski <sil16005@byui.edu>
2022-01-13 13:36:02 -0700

    Completed ta02 + stretch challenges

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,54 @@
 const express = require('express');
 const router = express.Router();
 
+let userList = [];
+let displayError = false;
+let errorMessage = '';
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: userList,
+    error: {display: displayError, message: errorMessage}
   });
+});
+
+const userExists = (username) => {
+  for(let user of userList) {
+    if (user.userName == username) {
+      return true;
+    };
+  }
+  return false;
+}
+
+router.post('/addUser', (req, res, next) => {
+  let username = req.body.userName;
+  if(userExists(username)) {
+    displayError = true;
+    errorMessage = `'${username} Already Exists'`;
+    res.redirect('/ta02');
+  } else {
+    displayError = false;
+    userList.push({userName: username});
+    res.redirect('/ta02');
+  }
+});
+
+router.post('/removeUser', (req, res, next) => {
+  let username = req.body.removeName;
+  if(userExists(username)) {
+    displayError = false;
+    userList = userList.filter((user) => user.userName != username);
+    res.redirect('/ta02');
+  } else {
+    displayError = true;
+    errorMessage = `'${username} Does Not Exist'`;
+    res.redirect('/ta02');
+  }
 });
 
 module.exports = router;
